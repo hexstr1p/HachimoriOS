d7318f6c2e61f07a08cbcc7db5c126c8ee2d304e
hexstr1p <hexstr1p@protonmail.com>
2019-09-20 22:35:32 -0400

    kernel build success

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,11 @@
-void KernelMain(void* multiboot_structure, unsigned int magic_number) {
+void printf(char* str) {
+  unsigned short* VideoMemory = (unsigned short*)0xb8000;
+  for (int i= 0; str[i] != '\0'; ++i) {
+    VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+  }
+}
+
+extern "C" void KernelMain(void* multiboot_structure, unsigned int magic_number) {
   printf("Systems Ready!");
   while(true);
 }
